f5b7b03e7008de6c2f7719fe6ebb7da1f59c3e7b
Ben <flamedarkness666@gmail.com>
2019-09-29 18:09:05 -0400

    initial site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,34 @@
 
 // There will be four crystals displayed as buttons on the page.
-var crystals = ["crystal1, crystal2, crystal3, crystal4"];
+// varible will be crystal
 
 // The player will be shown a random number at the start of the game.
-var randomNumber
+// varible will be randomNumber
 
 // When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
-
 // Your game will hide this amount until the player clicks a crystal.
 // When they do click one, update the player's score counter.
-onclick update score
+// function add
 
 // The player wins if their total score matches the random number from the beginning of the game.
-totalScore === randomNumber
+// userScore === randomNumber
 
 // The player loses if their score goes above the random number.
-totalScore > randomNumber
+// userScore > randomNumber
 
 // The game restarts whenever the player wins or loses.
-resetGame
-
+// resetgame()
 
 // When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. 
 // Of course, the user's score (and score counter) will reset to zero.
-// The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+// userScore reset = 0
 
+// The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+// randomNumber reset to random number
 
 // Option 1 Game design notes
 // The random number shown at the start of the game should be between 19 - 120.
-Math.floor(Math.random() * 120) + 19;
+// Math.floor(Math.random() * 120) + 19;
 
 // Each crystal should have a random hidden value between 1 - 12.
-Math.floor(Math.random() * 12) + 1;
+// Math.floor(Math.random() * 12) + 1;
